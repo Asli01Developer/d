b5eaa9838c735887a8cdc0c5d43d04b455df4f3b
asliddindeveloper1 <asliddin123I@gamil.com>
2023-08-08 11:18:15 +0500

    git a random todo

--- bin/htt_get_post.dart
+++ bin/htt_get_post.dart
@@ -4,7 +4,7 @@ import 'package:htt_get_post/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
 void main(List<String> args) async {
-  String baseUrl = "https://dummyjson.com/todos/1";
+  String baseUrl = "https://dummyjson.com/todos/random";
   Uri uri = Uri.parse("$baseUrl");
   http.Response response2 = await http.get(uri);
   User todos = User.fromJson(jsonDecode(response2.body));
